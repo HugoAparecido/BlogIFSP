59eb8a41f46bf2a210dd8d55bc12733ffdc0d027
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-10-03 10:26:14 -0300

    Update usuario_formulario.php

--- usuario_formulario.php
+++ usuario_formulario.php
@@ -4,12 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Usuário | Projeto para Web com PHP</title>
+    <link rel="stylesheet" href="lib/boo">
 </head>
 <body>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <?php include 'includes/topo.php';?>
+                <?php include 'includes/topo.php'; ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <?php include 'includes/menu.php'; ?>
+            </div>
+            <div class="col-md-10" style="padding-top: 50px;">
+                <?php 
+                require_once 'includes/funcoes.php';
+                ?>
             </div>
         </div>
     </div>
