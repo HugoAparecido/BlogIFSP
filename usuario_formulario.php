59773e4aab7d6462a3e4cf6da81f468925744cc7
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-10-03 10:28:17 -0300

    Update usuario_formulario.php

--- usuario_formulario.php
+++ usuario_formulario.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Usuário | Projeto para Web com PHP</title>
-    <link rel="stylesheet" href="lib/boo">
+    <link rel="stylesheet" href="lib/bootstrap-4.2.1-dist/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container">
@@ -20,6 +20,10 @@
             <div class="col-md-10" style="padding-top: 50px;">
                 <?php 
                 require_once 'includes/funcoes.php';
+                require_once 'core/conexao_mysql.php';
+                require_once 'core/sql.php';
+                require_once 'core/mysql.php';
+                if(isset)
                 ?>
             </div>
         </div>
